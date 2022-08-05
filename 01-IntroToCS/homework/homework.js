cf4c56747238d9dbb26270202ac81fb7668c50d1
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2022-08-05 17:12:55 -0500

    subiendo segunda tarea

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -4,12 +4,24 @@ const { TestWatcher } = require("jest");
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  return parseInt(num, 2)
+  num = num.split ('').reverse ();
+  let resultado =0;
+ for (let i =0; i < num.length; i++ ){
+ resultado = resultado + Math.pow(2,i) * parseInt(num[i])
 }
+return resultado
+ 
+}
+
+
 function DecimalABinario(num) {
   // tu codigo aca
-  return num.toString(2);
-
+  let resultado = [];
+  while (num > 0){
+    resultado.push(num%2);
+    num =Math.floor(num/2);
+  }
+return resultado.reverse().join('') ;
 }
 
 
