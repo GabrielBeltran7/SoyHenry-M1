1c7c42a2bbdba8baf9a38117d895aaf11393324a
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2022-08-31 16:33:07 -0500

    08-Algoritmos-II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,24 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length <=1){
+    return array
+  }
+  const pivot = array.shift();
+  const left =[];
+  const rigth = [];
+
+  while (array.length){
+    const items = array.shift()
+    if(items <pivot){
+      left.push (items)
+
+    }else{
+      rigth.push(items)
+    }
+  }
+
+return quickSort(left).concat(pivot).concat(quickSort(rigth))
 }
 
 function mergeSort(array) {
@@ -14,7 +32,33 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+if (array.length<=1){
+  return array
+}
+const index = Math.floor(array.length /2)
+let left = array.splice (0,index)
+let rigth = array
+left = mergeSort(left)
+rigth =mergeSort(rigth)
 
+
+
+
+const  result =[]
+while(left.length && rigth.length){
+  if(left[0]<rigth[0]){
+  result.push(left.shift())
+  }
+  else {
+    result.push(rigth.shift())
+  }
+}
+if(left.length){
+  return result.concat(left)
+}
+else{
+  return result.concat(rigth)
+}
 }
 
 // No modificar nada debajo de esta línea
